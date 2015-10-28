84e2ad32676f5c21bbe67c7045443d24d7b901ac
Gary Marx <gary@zenpayroll.com>
2015-10-27 18:20:23 -0700

    added registry functionality to add gifts

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,24 +1,28 @@
 class GiftsController < ApplicationController
 
   def index
-   @gifts = Gift.all
-   @new_gift = Gift.new
+    @gifts = Gift.all
+    @new_gift = Gift.new
   end
   def create
-   @gift = Gift.create(gift_params)
-   redirect_to '/gifts'
+    @gift = Gift.create(gift_params)
+    redirect_to '/gifts'
   end
   def edit
-   @gift = Gift.find(gift_params[:id])
+    @gift = Gift.find(params[:id])
   end
   def update
-   @gift= Gift.find(gift_params[:id])
-   gift.update_attributes(gift_params)
+    @gift= Gift.find(params[:id])
+    @gift.update_attributes(gift_params)
+    redirect_to '/registry'
+  end
+  def show
+    @gift = Gift.find(params[:id])
   end
 
   private
 
   def gift_params
-   params.require(:gift).permit(:item)
+    params.require(:gift).permit(:name, :url, :purchased)
   end
 end
