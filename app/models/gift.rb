f51ff7dfb4dce233919757ecb3bf5d41498f4c0a
Gary Marx <gary@zenpayroll.com>
2015-10-29 12:50:43 -0700

    modified gift view

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,6 +2,6 @@ class Gift < ActiveRecord::Base
   # add in w/ Devise users
   # belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/macys.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" } #, default_url: "/images/macys.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
