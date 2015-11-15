fcf57b52063c6dfe5f47c3344a2a935aeed8c03f
Gary <gamarx@gmail.com>
2015-11-15 00:03:17 -0800

    updated our story, fonts, css, removed maps from location page to simplify and more

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     [
       {text: 'Home', path: root_path},
       {text: 'Welcome', path: welcome_path},
+      {text: 'Our Story', path: about_path},
       {text: 'Wedding Day', path: wedding_path},
       {text: 'Accommodations', path: accommodations_path},
       {text: 'Location', path: location_path},
