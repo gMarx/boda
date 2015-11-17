6ae8dffaa77f5d303f050a010471f07dc12e3305
Gary <gamarx@gmail.com>
2015-11-16 23:25:49 -0800

    added 'description' to nav_links to pull as an 'id' for the elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def nav_links
     [
-      {text: 'Home', path: root_path},
-      {text: 'Welcome', path: welcome_path},
-      {text: 'Our Story', path: about_path},
-      {text: 'Wedding Day', path: wedding_path},
-      {text: 'Accommodations', path: accommodations_path},
-      {text: 'Location', path: location_path},
-      {text: 'Gift Registry', path: registry_path},
+      {text: 'Home', path: root_path, description: 'Home'},
+      {text: 'Welcome', path: welcome_path, description: 'Welcome'},
+      {text: 'Our Story', path: about_path, description: 'Our_Story'},
+      {text: 'Wedding Day', path: wedding_path, description: 'Wedding_Day'},
+      {text: 'Accommodations', path: accommodations_path, description: 'Accommodations'},
+      {text: 'Location', path: location_path, description: 'Location'},
+      {text: 'Gift Registry', path: registry_path, description: 'Gift_Registry'},
     ]
   end
 end
